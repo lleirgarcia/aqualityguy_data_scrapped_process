2b450f19f2fd7f838cfa24747d48a06118f073bc
Lleir Garcia <lleirgarcia@gmail.com>
2024-06-13 23:46:25 +0200

    controller updated (wip)

--- form-ask-chatgpt/backend/src/controllers/askController.ts
+++ form-ask-chatgpt/backend/src/controllers/askController.ts
@@ -1,14 +1,24 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
 import { Request, Response } from 'express';
-import { getOpenAIResponse } from '../services/openaiService';
+import { OpenAIService } from '../services/openaiService';
 import { fetchFilesFromS3 } from '../services/s3Service';
+import { OpenAIResponse } from '../interfaces/OpenAIResponse';
+import { __dirname } from '../utils';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+
+const openaiService = new OpenAIService(process.env.OPENAI_API_KEY!);
 
 const askController = async (req: Request, res: Response) => {
     console.log("asking...");
     const { question } = req.body;
     try {
         const files = await fetchFilesFromS3();
-        const response = await getOpenAIResponse(question, files);
-        res.json({ question, response });
+        const responses: OpenAIResponse[] = await openaiService.getOpenAIResponse(question, files);
+        res.json({ question, responses });
     } catch (error) {
         console.error("Error processing question:", error);
         res.status(500).send(error);
---
+++ form-ask-chatgpt/backend/src/interfaces/OpenAIResponse.ts
@@ -0,0 +1,9 @@
+export interface File {
+    Key: string;
+    Body: any;
+}
+
+export interface OpenAIResponse {
+    item: any;
+    response: string;
+}
