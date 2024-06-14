d43600c044c7a706c2b4ab061c3e9f6276a2b54e
Lleir Garcia <lleirgarcia@gmail.com>
2024-06-14 21:06:31 +0200

    add compresiond and send email (right now commented for wip)

--- form-ask-chatgpt/backend/src/controllers/askController.ts
+++ form-ask-chatgpt/backend/src/controllers/askController.ts
@@ -6,18 +6,27 @@ import { OpenAIService } from '../services/openaiService';
 import { fetchFilesFromS3 } from '../services/s3Service';
 import { OpenAIResponse } from '../interfaces/OpenAIResponse';
 import { __dirname } from '../utils';
+import { compressFiles } from '../services/compressionService';
+import { sendEmail } from '../services/emailService';
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
 
 const openaiService = new OpenAIService(process.env.OPENAI_API_KEY!);
 
 const askController = async (req: Request, res: Response) => {
     console.log("asking...");
-    const { question } = req.body;
+    const { question, email } = req.body;
     try {
         const files = await fetchFilesFromS3();
         const responses: OpenAIResponse[] = await openaiService.getOpenAIResponse(question, files);
+
+        // Compress the files
+        const compressedFilePath = await compressFiles();
+
+        // Send email with the compressed file (desactivated now)
+        // await sendEmail(email, compressedFilePath);
+
+        console.log("Ask finish")
         res.json({ question, responses });
     } catch (error) {
         console.error("Error processing question:", error);
