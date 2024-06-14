17e2de977dc5801b74333408fddb2b9d6bb4972e
Lleir Garcia <lleirgarcia@gmail.com>
2024-06-14 21:07:59 +0200

    front updated and email added

--- form-ask-chatgpt/frontend/src/App.tsx
+++ form-ask-chatgpt/frontend/src/App.tsx
@@ -7,12 +7,13 @@ import './App.css';
 const ChatForm: React.FC = () => {
     const [question, setQuestion] = useState<string>('');
     const [response, setResponse] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
     const [videoCount, setVideoCount] = useState<string>('5');
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         try {
-            const res = await axios.post('http://localhost:3001/ask', { question, videoCount });
+            const res = await axios.post('http://localhost:3001/ask', { question, email, videoCount });
             setResponse(JSON.stringify(res.data, null, 2));
         } catch (error) {
             console.error('Error al llamar a la API:', error);
@@ -22,6 +23,10 @@ const ChatForm: React.FC = () => {
 
     const handleInputChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
         setQuestion(event.target.value);
+    };
+
+    const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
     };
 
     const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
@@ -40,6 +45,13 @@ const ChatForm: React.FC = () => {
                         onChange={handleInputChange}
                         required
                     />
+                    <input
+                        type="email"
+                        placeholder="Email donde recibir respuestas..."
+                        value={email}
+                        onChange={handleEmailChange}
+                        required
+                    />
                     <select value={videoCount} onChange={handleSelectChange}>
                         <option value="5">5 últimos videos</option>
                         <option value="10">10 últimos videos</option>
@@ -49,9 +61,9 @@ const ChatForm: React.FC = () => {
                     <button type="submit">Hacer pregunta</button>
                 </form>
             </div>
-            <div className="Response" role="textbox" aria-multiline="true">
+            {/* <div className="Response" role="textbox" aria-multiline="true">
                 Respuesta: {response}
-            </div>
+            </div> */}
         </div>
     );
 }
