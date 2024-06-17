a73e68ed0d0dd4c3eaead0a5cfd8a5f7e9e58414
Lleir Garcia <lleirgarcia@gmail.com>
2024-06-17 20:38:44 +0200

    compresion and upload in controller and send email

--- form-ask-chatgpt/backend/src/controllers/askController.ts
+++ form-ask-chatgpt/backend/src/controllers/askController.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 import path from 'path';
-
 import { Request, Response } from 'express';
 import { OpenAIService } from '../services/openaiService';
 import { fetchFilesFromS3 } from '../services/s3Service';
 import { OpenAIResponse } from '../interfaces/OpenAIResponse';
 import { __dirname } from '../utils';
 import { compressFiles } from '../services/compressionService';
-import { sendEmail } from '../services/emailService';
+import { uploadFileToS3 } from '../services/s3Service';
+import { sendEmail } from 'services/emailService';
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
@@ -18,16 +18,14 @@ const askController = async (req: Request, res: Response) => {
     const { question, email } = req.body;
     try {
         const files = await fetchFilesFromS3();
-        const responses: OpenAIResponse[] = await openaiService.getOpenAIResponse(question, files);
+        // const responses: OpenAIResponse[] = await openaiService.getOpenAIResponse(question, files);
 
-        // Compress the files
-        const compressedFilePath = await compressFiles();
-
-        // Send email with the compressed file (desactivated now)
-        // await sendEmail(email, compressedFilePath);
-
-        console.log("Ask finish")
-        res.json({ question, responses });
+        const zipFilePath = await compressFiles(email);
+        const s3Key = `dataGenerated/historic/compressed_files_${email}.zip`;
+        const downloadLink = await uploadFileToS3(zipFilePath, s3Key);
+        console.log(`Links ${downloadLink}`)
+        await sendEmail(email, downloadLink);
+        res.json({ question, undefined, downloadLink });
     } catch (error) {
         console.error("Error processing question:", error);
         res.status(500).send(error);
